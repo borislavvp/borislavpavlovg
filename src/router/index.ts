39fb867ab9f45de8bfdda34fbdf8f132d7a2a6c0
borislavvp <46525030+borislavvp@users.noreply.github.com>
2025-12-08 00:51:31 +0800

    fix import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ModelHome from '@/views/ModelHome.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/ModelHome.vue')
+      component: ModelHome
     },
     {
       path: '/obrazcov-dom',
@@ -14,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ModelHome.vue')
+      component: ModelHome
     },
     {
       path: '/baobab',
